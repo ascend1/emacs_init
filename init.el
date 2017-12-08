5fffb655dc18a14af8c32077d13d51bd47dccc79
Zhen <Zhen Li>
2017-12-08 01:05:53 +0100

    * add company-ghc

--- init.el
+++ init.el
@@ -56,6 +56,12 @@
 (define-key c-mode-map [(tab)] 'company-complete)
 (define-key c++-mode-map [(tab)] 'company-complete)
 
+;; company-ghc
+(add-hook 'haskell-mode-hook 'company-mode)
+(require 'company-ghc)
+(add-to-list 'company-backends 'company-ghc)
+(custom-set-variables '(company-ghc-show-info t))
+
 ;; flycheck
 (add-hook 'c++-mode-hook 'flycheck-mode)
 (add-hook 'c-mode-hook 'flycheck-mode)
@@ -94,3 +100,17 @@
 (require 'smartparens-config)
 (add-hook 'c++-mode-hook #'smartparens-mode)
 (add-hook 'c-mode-hook #'smartparens-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company-ghc company-irony haskell-mode smartparens magit flycheck-rtags company-rtags company-irony-c-headers))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
